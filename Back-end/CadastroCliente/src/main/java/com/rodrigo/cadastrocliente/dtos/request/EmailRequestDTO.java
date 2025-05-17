296fcb3e88e6674554aeed6138a70ff8b270499b
Rodrigodx <rodrigodx52@gmail.com>
2025-05-17 18:07:43 -0300

    Adicionado validação de email no EmailRequestDTO

--- Back-end/CadastroCliente/src/main/java/com/rodrigo/cadastrocliente/dtos/request/EmailRequestDTO.java
+++ Back-end/CadastroCliente/src/main/java/com/rodrigo/cadastrocliente/dtos/request/EmailRequestDTO.java
@@ -6,14 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class EmailRequestDTO {
 
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
-
+    
+    @Valid
     private Cliente cliente;
 
 }
