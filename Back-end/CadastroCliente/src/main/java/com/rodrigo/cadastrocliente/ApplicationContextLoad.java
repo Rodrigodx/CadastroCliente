8ec986ec7e643524de8127d64491cb2e6f4eb395
Rodrigodx <rodrigodx52@gmail.com>
2025-05-18 14:05:01 -0300

    Criado classe contextLoad

---
+++ Back-end/CadastroCliente/src/main/java/com/rodrigo/cadastrocliente/ApplicationContextLoad.java
@@ -0,0 +1,27 @@
+package com.rodrigo.cadastrocliente;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class ApplicationContextLoad implements ApplicationContextAware {
+
+    @Autowired
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
