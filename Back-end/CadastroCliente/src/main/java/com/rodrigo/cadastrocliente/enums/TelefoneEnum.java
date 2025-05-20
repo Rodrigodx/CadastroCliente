4a259c13b6ed624a003407cbcbf92a8f9d886ce9
Rodrigodx <rodrigodx52@gmail.com>
2025-05-20 00:42:21 -0300

    Corrigido nome de enum

--- Back-end/CadastroCliente/src/main/java/com/rodrigo/cadastrocliente/enums/TelefoneEnum.java
+++ Back-end/CadastroCliente/src/main/java/com/rodrigo/cadastrocliente/enums/TelefoneEnum.java
@@ -1,11 +1,10 @@
 package com.rodrigo.cadastrocliente.enums;
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Arrays;
 import java.util.Optional;
 
 public enum TelefoneEnum {
 
-    RESIDENDCIAL(1),
+    RESIDENCIAL(1),
     COMERCIAL(2),
     CELULAR(3);
 
